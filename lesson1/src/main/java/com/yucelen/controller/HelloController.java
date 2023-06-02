fd1976a6be4e39457ed678a1fc21ba0fab0e9201
yucelenn <yucelenn@hotmail.com>
2023-06-02 14:46:09 +0300

    RequestMapping GetMapping

--- lesson1/src/main/java/com/yucelen/controller/HelloController.java
+++ lesson1/src/main/java/com/yucelen/controller/HelloController.java
@@ -1,13 +1,18 @@
 package com.yucelen.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path="/api") //localhost:8080/api
 public class HelloController {
 	
-	//localhost:8080/hello
+	//localhost:8080/api/hello
 	
+	
+	//@RequestMapping(path="/api" , method = RequestMethod.GET)
 	@GetMapping(path="/hello")
 	public String sayHello() {
 		return "Hello World";
